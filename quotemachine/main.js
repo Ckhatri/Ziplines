b14b64dbedceb94abfb3a61e5329a77aaad96436
ckhatri <chirag.k.khatri@gmail.com>
2015-07-01 15:41:27 -0500

    Quote Machine added, submodule removed

---
+++ quotemachine/main.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+	$("button").click(function() {
+	$.ajax({
+		url: 'http://quotesondesign.com/api/3.0/api-3.0.json',
+		type: 'GET',
+		dataType: "jsonp",
+		success: function(response) {
+			console.log(response);
+			var authorName = response.author;
+			var quote = response.quote;
+			console.log(authorName);
+			console.log(quote);
+			$('.saying').text(quote);
+			$('.author').text(authorName);
+		},
+		error: function(response) {
+			alert('fuck');
+		}
+	});
+	});
+});
+
