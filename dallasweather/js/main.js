760a36f8e2f6d2ac2ce94ebddfed04da304573a1
ckhatri <chirag.k.khatri@gmail.com>
2015-07-02 11:20:27 -0500

    Got the icon to show up based on api call

--- dallasweather/js/main.js
+++ dallasweather/js/main.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
 	console.log("HEYO");
 	$.ajax({
-
 		url: 'http://api.openweathermap.org/data/2.5/weather?id=4684888&units=imperial',
 		type: 'GET',
 		dataType: "jsonp",
@@ -12,16 +11,29 @@ $( document ).ready(function() {
 			var currWeather = response.weather;
 			var weatherInfo = currWeather[0];
 			var currentDescription = weatherInfo.description;
+			var iconVal = weatherInfo.icon;
 			var currentMain = weatherInfo.main;
 			var windInfo = info.wind;
 			var windSpeed = windInfo.speed;
 			var windDir = windInfo.deg;
 			$('#desc').text(currentDescription);
 			$('#wind').text(windSpeed + " mph " + windDir + "°");
-			console.log(info);
-			console.log(windInfo);
-			console.log(windSpeed);
-			console.log(windDir);
+			$('.temp').text(temp + "°");
+			var iconDiv = document.getElementById("weatherIcon");
+			var weatherIcon = new Image();
+			weatherIcon.onload = function () {
+				iconDiv.appendChild(weatherIcon);
+			};
+			var source = "http://openweathermap.org/img/w/" + iconVal + ".png";
+			// console.log(source);
+			weatherIcon.src = source;
+			console.log(weatherIcon.src);
+			// console.log(weatherIcon);
+			// console.log(info);
+			// console.log(windInfo);
+			// console.log(windSpeed);
+			// console.log(windDir);
+			// console.log(iconVal);
 
 		},
 		error: function(response) {
